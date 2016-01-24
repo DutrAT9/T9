48aa7edc7c7c9135cba1619804828ce60536014f
gabrielD9 <gabrieldutra-08@outlook.com>
2016-01-24 00:47:04 -0200

    Update T9.py

--- T9.py
+++ T9.py
@@ -1,5 +1,7 @@
+#Openodulos
 import os
 import socket
+#CloseModulos
 
 print('''##########################################################
 # [+] AUTOR:        Gabriel de Moura Dutra               #
@@ -8,24 +10,30 @@ print('''##########################################################
 # [+] FACEBOOK:     https://fb.com/gabriel.dutra.47884754#
 ##########################################################''')
 
+#Define a operacao
 print("1 : Add repositorios do backbox")
 print("2 : Instalar ferramentas de pentest")
 print("3 : PortScan")
-print("4 : Sair")
-
+print("4 : Instalar o tor-browser")
+print("5 : Quit")
 operacao = int(input("Digite operacao? "))
 
+#RepositoriosBackbox
 if operacao == 1:
-
+	os.system("echo '# Kali linux repositories | Added by h4wPy\ndeb http://http.kali.org/kali sana main non-free contrib\ndeb http://security.kali.org/kali-security sana/updates main contrib non-free\ndeb http://repo.kali.org/kali kali-bleeding-edge main' >> /etc/apt/sources.list")
+	os.system("apt-key adv --keyserver pgp.mit.edu --recv-keys ED444FF07D8D0BF6")
 	os.system("echo '# Backbox repositorios| Added by h4wPy\ndeb http://ppa.launchpad.net/backbox/four/ubuntu trusty main\ndeb-src http://ppa.launchpad.net/backbox/four/ubuntu trusty main' >> /etc/apt/sources.list")
 	os.system('sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 78A7ABE1')
-	os.system('sudo apt-get update')
-	os.system('sudo apt-get upgrade')
+	os.system('sudo apt-get update -m')
+	
 
+#Instalar ferramentas de pentest
 elif operacao == 2:
-	os.system('sudo apt-get install sqlmap, attest, dnschef, dotdotpwn, fang, john, joomscan, kismet, logkeys, amap, armitage, atshell, owasp, nmap, zenmap, tor, fimap, nikto, setoolkit, openvas, w3af, chunch, reaver, ophcrack, aircrack-ng, beef, wireshark, scapy, etthercap, hydra, ')
+	print("sqlmap\nAttest\nDnschef\nDotdotpwn\nFang\nJohn the rider\nJoomscan\nKismet\nLogkeys\nAmap\nArmitage\nAtshell\nOwasp\nNmap\nZenmap\nTor\nFimap\nNikto\nSetoolkit\nOpenvas\nW3af\nChunch\nReaver\nOphcrack\nAircrack-ng\nBeef\nWireshark\nScapy\nEtthercap\nHydra")
+	os.system('sudo apt-get install sqlmap dnschef dotdotpwn fang john joomscan kismet logkeys amap armitage nmap zenmap tor fimap nikto setoolkit  w3af reaver ophcrack aircrack-ng beef wireshark scapy hydra')
 
 
+#PortScan
 elif operacao == 3:
 	host = input("Digite IP: ")
 	def site_porta(host, port):
@@ -44,7 +52,13 @@ elif operacao == 3:
 			print("{}  <<Porta aberta".format(port))
 
 elif operacao == 4:
-	print("BYE!!!!!")
+	os.system('sudo add-apt-repository -y ppa:webupd8team/tor-browser')
+	os.system('sudo apt-get update')
+	os.system('sudo apt-get install tor-browser')
+
+#Close
+elif operacao == 5:
+	print("BYE!!!!!!")
 	os.system('clear')
 	os.system('exit')
 
